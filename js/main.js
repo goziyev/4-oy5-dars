8c5b8f623a073feb12357ac9f12a0781338e686a
goziyev <goziyevmuhammadgozi@gmail.com>
2023-12-04 17:51:55 +0500

    oddiy misollar tugadi

---
+++ js/main.js
@@ -0,0 +1,119 @@
+
+// 1-misol
+// let a  =  +prompt("son kiriting ");
+// let sum  = 0;
+// for (let index = 0; index <= a; index++) {
+//     if (index % 2 == 1) {
+//         sum+=index;
+//     }}
+//     console.log(sum);
+
+// 2-misol
+// let a  =  +prompt("son kiriting ");
+// let sum  = 0;
+// for (let index = 0; index <= a; index++) {
+//     if (index % 3 == 0) {
+//         sum*=index;
+//     }}
+//     console.log(sum);
+
+// // 3-misol
+// let a  =  +prompt("son kiriting ");
+// let b  =  +prompt("son kiriting ");
+// let sum  = 0;
+// for (let index = a; index <= b; index++) {
+//     if (index % 3 == 0) {
+//         sum++
+//     }}1
+//     console.log(sum);
+
+// 4-misol
+// let a  =  +prompt("son kiriting ");
+// let b  =  +prompt("son kiriting ");
+// let sum  = 0;
+// for (let index = a; index <= b; index++) {
+//     if (index % 3 == 0 && index % 7 == 0 ) {
+//         sum+=index
+//     }}1
+//     console.log(sum);
+
+// 5-misol
+// let a  =  +prompt("1-son kiriting ");
+// let b  =  +prompt("2-son kiriting ");
+// let sum  = 0;
+// let total = 0;
+// for (let index = a; index <= b; index++) {
+//     if (index % 2 == 1) {
+//         sum = index ** 2;
+//         total += sum;
+//         }
+//     }
+    
+//     console.log(total);
+
+//6-misol
+// let a  =  +prompt("1-son kiriting ");
+// let b  =  +prompt("2-son kiriting ");
+// let sum  = 0;
+// let total = 1;
+// for (let index = a; index <= b; index++) {
+//     if (index % 2 == 0) {
+//         sum = index ** 3;
+//         total *= sum;
+//         }
+//     }
+    
+//     console.log(total);
+
+// 7-misol
+// let a = +prompt("Marhamat son kiriting: ");
+// for (let i = 0; i <= a; i++) {
+//     if (a % i == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 8-misol
+// let a = +prompt("Marhamat son kiriting: ");
+// let count = 0
+// for (let i = 0; i <= a; i++) {
+//     if (a % i == 0) {
+//         count++
+//     }
+// }
+// console.log(count);
+
+// 9-misol
+// let a = +prompt("Marhamat son kiriting: ");
+// let count = 0;
+// let res = ""
+// for (let i = 0; i <= a; i++) {
+//     if (a % i == 0) {
+//         count++
+//     }
+// }
+// if (count <= 2) {
+//     res = "Bu tub son"
+// }
+// else{
+//     res = "Tub son emas"
+// }
+
+// console.log(res);
+
+// Vaqt
+let a = +prompt("Marhamat qo'shmoqchi bo'lgan vaqtingizni kirting:");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
